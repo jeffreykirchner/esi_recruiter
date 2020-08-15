7c1178a98126ba416f82c91bc1a5e186d0ce7653
Jeffrey Kirchner <jkirchner@gmail.com>
2020-08-15 11:39:59 -0700

    replace with dedicated parameter model

--- main/forms/__init__.py
+++ main/forms/__init__.py
@@ -6,6 +6,5 @@ from .verifyFormResend import *
 from .userInfoForm import *
 from .experimentForm1 import *
 from .recruitmentParametersForm import *
-from .experimentSessionForm1 import *
 from .experimentSessionForm2 import *
 from .parametersForm import *
