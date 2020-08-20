104376ae9cb231a10e3841885a5d2c3f494df8fb
Jeffrey Kirchner <jkirchner@gmail.com>
2020-08-20 16:54:29 -0700

    Send message to all active users dev

--- main/views/staff/userSearch.py
+++ main/views/staff/userSearch.py
@@ -24,75 +24,96 @@ def userSearch(request):
         data = json.loads(request.body.decode('utf-8'))
 
         if data["action"] == "getUsers":
-            logger = logging.getLogger(__name__)
-            logger.info("User Search")
-            logger.info(data)
-
-            request.session['userSearchTerm'] = data["searchInfo"]            
-            activeOnly = data["activeOnly"] 
-
-            users = lookup(data["searchInfo"],False,activeOnly)            
-
-            errorMessage=""
-
-            if(len(users) >= 1000):
-                errorMessage = "Narrow your search"
-                users=[]          
-
-            return JsonResponse({"users" : json.dumps(users,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+            return getUsers(request,data)
         elif data["action"] == "getBlackBalls":
-            logger = logging.getLogger(__name__)
-            logger.info("Black Balls")
-            logger.info(data)
-
-            errorMessage=""
-            activeOnly = data["activeOnly"] 
-
-            users=User.objects.order_by(Lower('last_name'),Lower('first_name')) \
-                      .filter(profile__blackballed=True) \
-                      .select_related('profile') \
-                      .values("id","first_name","last_name","email","profile__chapmanID","profile__type__name","is_active","profile__blackballed")
-           
-            if activeOnly:
-                users = users.filter(is_active = True)
-
-            u_list = list(users)
-
-            return JsonResponse({"users" :  json.dumps(u_list,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+            return getBlackBalls(request,data)
         elif data["action"] == "getNoShows":
-            logger = logging.getLogger(__name__)
-            logger.info("Get no show blocks")
-            logger.info(data)
-
-            p = parameters.objects.get(id=1)
-            d = datetime.now(timezone.utc) - timedelta(days=p.noShowCutoffWindow)
-
-            errorMessage = ""
-            activeOnly = data["activeOnly"] 
-
-            users=User.objects.order_by(Lower('last_name'),Lower('first_name')) \
-                      .filter(Q(ESDU__confirmed = True) &
-                            Q(ESDU__attended = False) &
-                            Q(ESDU__bumped = False) & 
-                            Q(ESDU__experiment_session_day__experiment_session__canceled = False) &
-                            Q(ESDU__experiment_session_day__date__gte = d))\
-                      .annotate(noShows = Count('id'))\
-                      .filter(noShows__gte = p.noShowCutoff)\
-                      .values("id","first_name","last_name","email","profile__chapmanID","profile__type__name","is_active","profile__blackballed")
-
-            #logger.info(users.query)
-
-            if activeOnly:
-                users = users.filter(is_active = True)
-
-            u_list = list(users)
-
-            return JsonResponse({"users" :  json.dumps(u_list,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+            return getNoShows(request,data)
+        elif data["action"] == "sendEmail":
+            return sendEmail(request,data)
 
     else:
         activeCount = User.objects.filter(is_active = True,profile__type__id = 2).count()
         return render(request,'staff/userSearch.html',{"activeCount":activeCount})     
 
+#send an email to active users
+def sendEmail(request,data):
+    pass
+
+
+
+#get a list of no show violators
+def getNoShows(request,data):
+    logger = logging.getLogger(__name__)
+    logger.info("Get no show blocks")
+    logger.info(data)
+
+    p = parameters.objects.get(id=1)
+    d = datetime.now(timezone.utc) - timedelta(days=p.noShowCutoffWindow)
+
+    errorMessage = ""
+    activeOnly = data["activeOnly"] 
+
+    users=User.objects.order_by(Lower('last_name'),Lower('first_name')) \
+                .filter(Q(ESDU__confirmed = True) &
+                    Q(ESDU__attended = False) &
+                    Q(ESDU__bumped = False) & 
+                    Q(ESDU__experiment_session_day__experiment_session__canceled = False) &
+                    Q(ESDU__experiment_session_day__date__gte = d))\
+                .annotate(noShows = Count('id'))\
+                .filter(noShows__gte = p.noShowCutoff)\
+                .values("id","first_name","last_name","email","profile__chapmanID","profile__type__name","is_active","profile__blackballed")
+
+    #logger.info(users.query)
+
+    if activeOnly:
+        users = users.filter(is_active = True)
+
+    u_list = list(users)
+
+    return JsonResponse({"users" :  json.dumps(u_list,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+
+#get a list of all active black ball users
+def getBlackBalls(request,data):
+    logger = logging.getLogger(__name__)
+    logger.info("Black Balls")
+    logger.info(data)
+
+    errorMessage=""
+    activeOnly = data["activeOnly"] 
+
+    users=User.objects.order_by(Lower('last_name'),Lower('first_name')) \
+                .filter(profile__blackballed=True) \
+                .select_related('profile') \
+                .values("id","first_name","last_name","email","profile__chapmanID","profile__type__name","is_active","profile__blackballed")
+    
+    if activeOnly:
+        users = users.filter(is_active = True)
+
+    u_list = list(users)
+
+    return JsonResponse({"users" :  json.dumps(u_list,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+
+#return list of users based on search criterion
+def getUsers(request,data):
+    logger = logging.getLogger(__name__)
+    logger.info("User Search")
+    logger.info(data)
+
+    #request.session['userSearchTerm'] = data["searchInfo"]            
+    activeOnly = data["activeOnly"] 
+
+    users = lookup(data["searchInfo"],False,activeOnly)            
+
+    errorMessage=""
+
+    if(len(users) >= 1000):
+        errorMessage = "Narrow your search"
+        users=[]          
+
+    return JsonResponse({"users" : json.dumps(users,cls=DjangoJSONEncoder),"errorMessage":errorMessage},safe=False)
+
+#search for users that back search criterion
 def lookup(value,returnJSON,activeOnly):
     logger = logging.getLogger(__name__)
     logger.info("User Lookup")
