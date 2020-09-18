6751146be24b3eba95d0bf90657a385d301c8f18
Jeffrey Kirchner <jkirchner@gmail.com>
2020-09-18 10:58:07 -0700

    dev server setup

--- main/views/profileCreate.py
+++ main/views/profileCreate.py
@@ -38,8 +38,8 @@ def profileCreate(request):
 
     #migrate_session_users1()
     #migrate_session_users2()  #***
-    migrate_session_users4()  #***
-    migrate_session_users3()  #***
+    #migrate_session_users4()  #***
+    #migrate_session_users3()  #***
 
     migrate_parameters() #***
 
