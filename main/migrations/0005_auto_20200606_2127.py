bdb857b8329dea40da89f54c5a5c312588393d99
Jeffrey Kirchner <jkirchner@gmail.com>
2020-09-18 09:14:30 -0700

    remove addition of id field

--- main/migrations/0005_auto_20200606_2127.py
+++ main/migrations/0005_auto_20200606_2127.py
@@ -12,12 +12,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='profile',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
-            preserve_default=False,
-        ),
+        # migrations.AddField(
+        #     model_name='profile',
+        #     name='id',
+        #     field=models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
+        #     preserve_default=False,
+        # ),
         migrations.AlterField(
             model_name='profile',
             name='user',
