6511803d81e402081236e0c2e5752aa14ef684b9
Jeffrey Kirchner <jkirchner@gmail.com>
2020-12-13 14:28:49 -0800

    development

--- main/views/staff/__init__.py
+++ main/views/staff/__init__.py
@@ -8,3 +8,4 @@ from .experimentSessionPayoutsView import *
 from .calendarView import *
 from .experimentSearchView import *
 from .reportsView import *
+from .traits import *
---
+++ main/views/staff/traits.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from main.decorators import user_is_staff
+import json
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+import logging
+
+@login_required
+@user_is_staff
+def traitsView(request):
+    logger = logging.getLogger(__name__) 
+    
+    # logger.info("some info")
+
+    if request.method == 'POST':       
+
+        data = json.loads(request.body.decode('utf-8'))
+
+        if data["action"] == "action1":
+            pass
+        elif data["action"] == "action2":
+            pass
+           
+            return JsonResponse({"response" :  "some json"},safe=False)       
+    else:      
+        return render(request,'staff/traits.html',{"u":None ,"id":None})      
