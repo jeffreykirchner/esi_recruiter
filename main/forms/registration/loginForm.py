05290f06a456d8007cc9862dcdb1f7764de6b1be
Jeffrey Kirchner <jkirchner@gmail.com>
2021-01-08 10:45:29 -0800

    press "enter" to log in

--- main/forms/registration/loginForm.py
+++ main/forms/registration/loginForm.py
@@ -8,4 +8,4 @@ class loginForm(forms.Form):
     username =  forms.EmailField(label='Email address (lower case)')
 
     password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput(attrs={}))               
+                               widget=forms.PasswordInput(attrs={"v-on:keyup.enter":"login"}))               
