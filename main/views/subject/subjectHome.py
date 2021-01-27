bccee922b994a4c372e2be5564b99251926ca58c
Jeffrey Kirchner <jkirchner@gmail.com>
2021-01-26 17:20:16 -0800

    Update loggers

--- main/views/subject/subjectHome.py
+++ main/views/subject/subjectHome.py
@@ -112,7 +112,7 @@ def acceptInvitation(data,u):
     :type u: django.contrib.auth.models.User
     '''
     logger = logging.getLogger(__name__)
-    logger.info("Accept invitation")    
+    logger.info(f"Accept invitation: user {u}")    
     logger.info(data)
 
     failed=False
@@ -126,31 +126,27 @@ def acceptInvitation(data,u):
         if qs:
             #subject cannot attend before consent form accepted
             if u.profile.consent_required:
-                logger.info("Consent required before accept") 
+                logger.warning(f"Consent required before accept, user: {u}") 
                 failed=True
 
             #check that session has not started
             if not failed:
                 if qs.hoursUntilFirstStart() <= -0.25:
-                    logger.info("Invitation failed session started")             
-                    logger.info("User: " + str(u.id))
-
+                    logger.warning(f"Invitation failed session started, user: {u}")            
                     failed=True
 
             #check session not already full
             if not failed:
                 if qs.getFull(): 
-                    logger.info("Invitation failed accept session full")             
-                    logger.info("User: " + str(u.id))
+                    logger.warning(f"Invitation failed accept session full, user: {u}")             
                     failed=True
             
             #check user is not already attending a recruitment violation
             if not failed:
                 user_list_valid = qs.getValidUserList_forward_check([{'id':u.id}],False,0,0,[],False,1)
-                logger.info( "Valid user list: " + str(user_list_valid))
+                logger.info(f"Accept Invitation Valid User List: {user_list_valid}")
                 if not u in user_list_valid:
-                    logger.info("Invitation failed recruitment violation")             
-                    logger.info("User: " + str(u.id))
+                    logger.warning(f"Invitation failed recruitment violation, user {u}")             
                     failed=True
             
             # #check that by attending this experiment, user will not create recruitment violation in another confirmed experiment
@@ -165,29 +161,27 @@ def acceptInvitation(data,u):
                                                 user__id=u.id).first()
                 
                 if esdu.getAlreadyAttended():
-                    logger.info("Invitation failed, user has already done this experiment")
-                    logger.info("User: " + str(u.id) + ", attending session: " + str(esdu.id))
+                    logger.warning("Invitation failed, user has already done this experiment")
+                    logger.warning(f"User: {u}, attending session: {esdu.id}")
                     failed=True
             
             #update confirmed status
             if not failed:
-                logger.info("Accept Success")
+                logger.info(f"Accept Success: {u} {esdu}")
                 experiment_session_day_users.objects.filter(experiment_session_day__experiment_session__id = qs.id,
                                                 user__id=u.id)\
                                         .update(confirmed=True)
             else:
-                logger.info("Accept Failed") 
+                logger.warning(f"Accept Failed {u}") 
 
         else:
-            logger.info("Invitation not found")             
-            logger.info("User: " + str(u.id))
+            logger.warning(f"Invitation not found {u} {esdu}")             
             failed=True
 
     except Exception  as e:
-        logger.info("Accept invitation error")             
-        logger.info("User: " + str(u.id))    
-        logger.info(e)
-
+        logger.warning(e)
+        logger.warning(f"Accept invitation error {u}")             
+       
         failed=True
 
     upcomingInvitations = u.profile.sorted_session_list_upcoming()
