a564ff7af595a66786080ba443cf3d2d92c59d77
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 15:01:08 -0700

    celery service

--- startup.sh
+++ startup.sh
@@ -2,10 +2,11 @@
 python manage.py migrate
 apt-get -y install htop
 apt-get -y install systemd
-/etc/init.d/dbus start
-cp celery.service /etc/systemd/system/
-systemctl daemon-reload
 mkdir /etc/conf.d/
 mkdir /var/run/celery/
 mkdir /var/log/celery/
+/etc/init.d/dbus start
+cp celery.service /etc/systemd/system/celery.service
+cp celery /etc/conf.d/celery
+systemctl daemon-reload
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
