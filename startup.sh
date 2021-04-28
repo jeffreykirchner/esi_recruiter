c323dc2d2e629338ce8ae9d85b04bb25ad61f149
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 14:22:27 -0700

    install htop at startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
+\#!/bin/bash
 python manage.py migrate
-celery -A ESIRecruiter purge -f
-celery -A ESIRecruiter worker -E -l INFO --time-limit 120 &
-celery -A ESIRecruiter beat -l INFO -S django_celery_beat.schedulers:DatabaseScheduler &
+apt-get -y install htop
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
