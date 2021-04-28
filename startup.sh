33022bea2f24002888d182b3b3058b3efb72838e
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 10:28:08 -0700

    update celery scheduler

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 celery -A ESIRecruiter worker --loglevel=INFO -n worker1@%h --logfile /home/site/logs/chapman-experiments-dev/celery_worker.log --detach
-celery -A ESIRecruiter beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --logfile /home/site/logs/chapman-experiments-dev/celery_beat.log -s /home/site/logs/chapman-experiments-dev/celerybeat-schedule --detach
+celery -A ESIRecruiter beat -l INFO -S django --logfile /home/site/logs/chapman-experiments-dev/celery_beat.log -s /home/site/logs/chapman-experiments-dev/celerybeat-schedule --detach
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
