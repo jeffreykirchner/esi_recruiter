02260b59a320d4aecc1882fa415c46f58e553502
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 15:02:22 -0700

    celery service

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ apt-get -y install systemd
 mkdir /etc/conf.d/
 mkdir /var/run/celery/
 mkdir /var/log/celery/
-/etc/init.d/dbus start
 cp celery.service /etc/systemd/system/celery.service
 cp celery /etc/conf.d/celery
+/etc/init.d/dbus start
 systemctl daemon-reload
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
