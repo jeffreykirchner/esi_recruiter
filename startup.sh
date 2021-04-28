a6a5f1776c2d3ba5e1aad9f0969db52a5ab5a8d3
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 11:06:56 -0700

    remove worker scheduler

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 python manage.py migrate
 celery -A ESIRecruiter purge -f
-celery -A ESIRecruiter worker -E -l INFO -S django --time-limit 120 --logfile /home/site/logs/chapman-experiments-dev/celery_worker.log --detach
+celery -A ESIRecruiter worker -E -l INFO --time-limit 120 --logfile /home/site/logs/chapman-experiments-dev/celery_worker.log --detach
 celery -A ESIRecruiter beat -l INFO -S django --logfile /home/site/logs/chapman-experiments-dev/celery_beat.log -s /home/site/logs/chapman-experiments-dev/celerybeat-schedule --detach
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
