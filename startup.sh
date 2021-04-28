15005918fb184a29689ff23a49142a8ad650e6a1
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 12:27:26 -0700

    update celery startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 python manage.py migrate
 celery -A ESIRecruiter purge -f
 celery -A ESIRecruiter worker -E -l INFO --time-limit 120 --logfile /home/site/logs/chapman-experiments-dev/celery_worker.log --detach
-celery -A ESIRecruiter beat -l INFO -S django --logfile /home/site/logs/chapman-experiments-dev/celery_beat.log --detach
+celery -A ESIRecruiter beat -l INFO -S django_celery_beat.schedulers:DatabaseScheduler --logfile /home/site/logs/chapman-experiments-dev/celery_beat.log --detach
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
