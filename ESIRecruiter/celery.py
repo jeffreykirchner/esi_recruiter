0c40c36e6c5e384ddcd9eede7d3afdfc0f185d80
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-27 20:29:10 -0700

    define periodic task through admin

--- ESIRecruiter/celery.py
+++ ESIRecruiter/celery.py
@@ -27,13 +27,13 @@ app.autodiscover_tasks()
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    logger = logging.getLogger(__name__)
-    logger.info("Setup periodic tasks")
-    sender.add_periodic_task(crontab(minute='*/5'), run_crons.s(), name='Run crons every 5 minutes')
-    #sender.add_periodic_task(300, run_crons.s(), name='Run crons every 5 minutes')
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     logger = logging.getLogger(__name__)
+#     logger.info("Setup periodic tasks")
+#     sender.add_periodic_task(crontab(minute='*/5'), run_crons.s(), name='Run crons every 5 minutes')
+#     #sender.add_periodic_task(300, run_crons.s(), name='Run crons every 5 minutes')
 
 @app.task
 def run_crons():
