83ea4fa5f84e3ec8b0e1612274984fa8c8d64599
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 18:30:29 -0700

    update supervisor

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ apt-get -y install htop
 apt-get -y install supervisor
 mkdir /var/log/celery/
 mkdir /var/log/supervisord/
-cp supervisord.conf /etc/supervisor/conf.d/
+supervisord -c supervisord.conf
 supervisorctl reread
 supervisorctl update
-supervisord -c celeryd.conf
+supervisorctl -c supervisord.conf
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
