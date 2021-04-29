94a3baeb4c39a9a3a81b5964bc7d35515351b6b0
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-28 17:27:31 -0700

    systemd to supervisor

--- startup.sh
+++ startup.sh
@@ -1,12 +1,11 @@
 \#!/bin/bash
 python manage.py migrate
 apt-get -y install htop
-apt-get -y install systemd
-mkdir /etc/conf.d/
-mkdir /var/run/celery/
+apt-get -y install supervisor
 mkdir /var/log/celery/
-cp celery.service /etc/systemd/system/celery.service
-cp celery /etc/conf.d/celery
-/etc/init.d/dbus start
-systemctl daemon-reload
+mkdir /var/log/supervisord/
+cp supervisord.conf /etc/supervisor/conf.d/
+supervisorctl reread
+supervisorctl update
+supervisord -c celeryd.conf
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
