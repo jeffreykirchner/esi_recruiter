ca3f01530751a2c4c6f191a5ab284a667902809e
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-29 14:17:01 -0700

    disable celery

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 \#!/bin/bash
 python manage.py migrate
 apt-get -y install htop
-mkdir /var/log/celery/
-mkdir /var/log/supervisord/
-supervisord -c supervisord.conf
-supervisorctl -c supervisord.conf
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --max-requests-jitter 10  ESIRecruiter.wsgi
