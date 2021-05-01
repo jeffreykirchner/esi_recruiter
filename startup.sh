721aaa0bc192b6204925805e5a683e13025339e3
Jeffrey Kirchner <jkirchner@gmail.com>
2021-05-01 09:51:12 -0700

    add post build command, echos

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
+echo "*** Startup.sh ***"
+echo "Run Migrations:"
 python manage.py migrate
+echo "Install htop:"
 apt-get -y install htop
 gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --workers=2 --max-requests-jitter 10  ESIRecruiter.wsgi --access-logfile '-' --error-logfile '-'
