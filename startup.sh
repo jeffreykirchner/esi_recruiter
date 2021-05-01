0d94fe96c04dc50635d281ca614981c25ec88451
Jeffrey Kirchner <jkirchner@gmail.com>
2021-04-30 21:24:04 -0700

    update workers

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 apt-get -y install htop
-gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --workers=4 --max-requests-jitter 10  ESIRecruiter.wsgi --access-logfile '-' --error-logfile '-'
+gunicorn --bind=0.0.0.0 --timeout 1800 --max-requests 500 --workers=2 --max-requests-jitter 10  ESIRecruiter.wsgi --access-logfile '-' --error-logfile '-'
