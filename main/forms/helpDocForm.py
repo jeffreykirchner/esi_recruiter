249f9097c0179822da75eefafcbd132e968c1cd6
Jeffrey Kirchner <jkirchner@gmail.com>
2021-09-10 12:27:05 -0700

    sqlparse==0.4.2

--- main/forms/helpDocForm.py
+++ main/forms/helpDocForm.py
@@ -8,13 +8,13 @@ class helpDocForm(forms.ModelForm):
 
 
     title = forms.CharField(label='Title',
-                                         widget=forms.TextInput(attrs={"size":"125"}))
+                            widget=forms.TextInput(attrs={"size":"125"}))
     
     path = forms.CharField(label='URL Path',
-                                         widget=forms.TextInput(attrs={"size":"125"}))
+                           widget=forms.TextInput(attrs={"size":"125"}))
 
     text = forms.CharField(label='Text',
-                                     widget=forms.Textarea(attrs={"rows":"30", "cols":"125"}))
+                           widget=forms.Textarea(attrs={"rows":"30", "cols":"125"}))
 
 
     class Meta:
