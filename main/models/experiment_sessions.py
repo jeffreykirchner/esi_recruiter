1b3533d4f37bef3a8ce8eab2e497cb8b5a51f7b6
Jeffrey Kirchner <jkirchner@gmail.com>
2021-09-23 13:31:59 -0700

    show invitation text on subject screen

--- main/models/experiment_sessions.py
+++ main/models/experiment_sessions.py
@@ -1125,7 +1125,7 @@ class experiment_sessions(models.Model):
             "hours_until_first_start": self.hoursUntilFirstStart(),
             "full": self.getFull(),
             "valid" : False if not user_list_valid_check or not user_list_valid2_check else True,
-            "invitation_text" : session_invitation.messageText 
+            "invitation_text" : session_invitation.messageText.replace("[first name]", u.first_name)
                                 if (session_invitation := u.experiment_session_invitation_users.filter(experiment_session=self).first())
                                 else "Invitation text not found.",
         }
