d848c435621f079c711910081a2c29837ac48790
Jeffrey Kirchner <kirchner@chapman.edu>
2022-06-08 20:27:04 -0700

    add consent report on run screen.

--- main/templates/staff/experiment_session_payouts/pixi_setup.js
+++ main/templates/staff/experiment_session_payouts/pixi_setup.js
@@ -10,6 +10,59 @@ setupPixi(){
 
     app.$data.pixi_signature_texture = PIXI.Texture.from('{% static "signature_4.png" %}');
 
+    if(! app.$data.pixi_app)
+    {
+        app.$data.offScreenCanvas = document.createElement("canvas");
+        app.$data.c_width = 200;
+        app.$data.c_height = 50;
+        app.$data.offScreenCanvas.width = app.$data.c_width;
+        app.$data.offScreenCanvas.height = app.$data.c_height;
+
+        app.$data.pixi_app = new PIXI.Application({
+            
+            width:app.$data.c_width,
+            height:app.$data.c_height,
+
+            antialias: true,
+            
+
+            view: app.$data.offScreenCanvas });
+    }
+
+    app.$data.counter=0;
+    app.$data.tick_tock=0;
+
+    app.$data.pixi_app.ticker.add((delta) => {       
+
+        if(app.$data.counter<app.$data.sessionDayUsers.length)
+        {
+            if(app.$data.tick_tock==0)
+            {
+                app.$data.tick_tock=1;
+                app.resetPixiApp("signature_canvas_id_" + app.$data.sessionDayUsers[app.$data.counter].id, 
+                            app.$data.sessionDayUsers[app.$data.counter].profile_consent_form.signature_points,
+                            app.$data.sessionDayUsers[app.$data.counter].profile_consent_form.singnature_resolution)
+            }
+            else
+            {
+                app.$data.tick_tock=0;
+
+                let canvas = document.getElementById("signature_canvas_id_" + app.$data.sessionDayUsers[app.$data.counter].id);
+                temp_c=app.$data.pixi_app.renderer.plugins.extract.canvas(app.$data.pixi_app.stage)
+                canvas.getContext("2d").drawImage(temp_c, 0, 0);
+
+                app.$data.counter++;
+            }
+        }
+        else
+        {
+            app.$data.counter=0;
+            app.$data.tick_tock=0;
+        }
+
+    
+    });
+
     for(let i=0;i<app.$data.sessionDayUsers.length;i++)
     {
         // if(app.$data.sessionDayUsers[i].pixi_app)
@@ -17,23 +70,15 @@ setupPixi(){
         //     app.$data.sessionDayUsers[i].pixi_app.destroy();
         // }
         
-        app.$data.sessionDayUsers[i].pixi_app = app.resetPixiApp("signature_canvas_id_" + app.$data.sessionDayUsers[i].id, 
-                                                                 app.$data.sessionDayUsers[i].profile_consent_form.signature_points,
-                                                                 app.$data.sessionDayUsers[i].profile_consent_form.singnature_resolution)
+        
     }
 },
 
 resetPixiApp(canvas_id, signature_points, signature_scale){
     
-    let canvas = document.getElementById(canvas_id);
-
-    let pixi_app = new PIXI.Application({resizeTo : canvas,
-                                    backgroundColor : 0xFFFFFF,
-                                    autoResize: true,
-                                    antialias: true,
-                                    resolution: 1,
-                                    view: canvas });
+    app.$data.pixi_app.stage.removeChildren()
     
+    if(app.$data.payGroup=="consent")
     for (let i in signature_points) {
 
         let t = signature_points[i];        
@@ -48,12 +93,10 @@ resetPixiApp(canvas_id, signature_points, signature_scale){
             
         r = new PIXI.SimpleRope(app.$data.pixi_signature_texture, points);
 
-        r.scale.x = r.scale.y = canvas.height / signature_scale.height;
+        //r.scale.x = r.scale.y = app.$data.c_height / signature_scale.height;
 
-        pixi_app.stage.addChild(r);
-    }
-
-    pixi_app.destroy();
+        app.$data.pixi_app.stage.addChild(r);
+    }    
 },
 
 //load past signature
