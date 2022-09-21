bee8051ed6e72020060a6b8d06c306ddc4ec9372
Jeffrey Kirchner <kirchner@chapman.edu>
2022-09-21 14:32:50 -0700

    update gunicorn workers

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ echo "*** Startup.sh ***"
 echo "Run Migrations:"
 python manage.py migrate
 echo "Start gunicorn:"
-gunicorn --bind=0.0.0.0 --timeout 600 ESIRecruiter.wsgi --access-logfile '-' --error-logfile '-'
+gunicorn --bind=0.0.0.0 --timeout 600 ESIRecruiter.wsgi --access-logfile '-' --error-logfile '-' --workers=2
