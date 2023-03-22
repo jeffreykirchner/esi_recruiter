3d53b01fc40f00a7e288a673386d5b9221e73bee
Jeffrey Kirchner <jkirchner@gmail.com>
2023-03-22 15:56:58 -0700

    update roll-over text with trait descriptions. fixes #525.

--- main/forms/traitReportForm.py
+++ main/forms/traitReportForm.py
@@ -1,13 +1,15 @@
-from django import forms
-from main.models import Traits
-from django.db.models.functions import Lower
-
 import logging
 
-#form
+from django import forms
+from django.db.models.functions import Lower
+
+from main.models import Traits
+from main.models import HrefTraits
+
+# trait report form
 class traitReportForm(forms.Form):
     traits = forms.ModelMultipleChoiceField(label="",
-                                            queryset=Traits.objects.filter(archived=False),
+                                            queryset=HrefTraits.objects.filter(archived=False),
                                             widget = forms.CheckboxSelectMultiple(attrs={}))
 
 
--- main/models/traits.py
+++ main/models/traits.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from django.db.models.functions import Lower
+from django.utils.safestring import mark_safe
 
 class Traits(models.Model): 
     name = models.CharField(verbose_name='Name', max_length=300, unique=True)      #unique name of trait
@@ -25,3 +26,12 @@ class Traits(models.Model):
             "description":self.description,
             "archived":self.archived,
         }
+    
+#proxy model returns link to experiemnts
+class HrefTraits(Traits): 
+    class Meta:
+        proxy = True
+
+    def __str__(self):
+        # return mark_safe('<a href=\"/experiment/' + str(self.id) + '\" target=_self  >' + str(self.title) + '</a>')
+        return mark_safe(f'<a href="/admin/main/traits/{self.id}" title="{self.description}">{self.name}</a>')
