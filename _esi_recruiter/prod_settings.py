49fff0bf3d08c2ca3bf62ec62370348cdb23b21b
Jeffrey Kirchner <jkirchner@gmail.com>
2024-08-19 11:05:51 -0700

    add CONN_MAX_AGE to db connection settings. fixes #667.

--- _esi_recruiter/prod_settings.py
+++ _esi_recruiter/prod_settings.py
@@ -33,6 +33,7 @@ DATABASES = {
         'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASS'],
         'OPTIONS': {'sslmode': 'prefer'},
+        'CONN_MAX_AGE' : 60,
     },
 }
 
