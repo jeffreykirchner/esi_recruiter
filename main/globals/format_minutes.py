d77d5bd1fef9fbb6f86e413aebd56b73a6c73c8a
Jeffrey Kirchner <jkirchner@gmail.com>
2026-03-10 16:50:32 +0000

    fix: Capitalize last name in profile serialization and refactor format_minutes function for clarity

--- main/globals/format_minutes.py
+++ main/globals/format_minutes.py
@@ -11,25 +11,39 @@ def format_minutes(minutes) -> str:
     '''
     logger = logging.getLogger(__name__)
     
-    v = f'{math.floor(minutes/60)}'
+    # v = f'{math.floor(minutes/60)}'
 
-    if v == "1":
-        v += "hr "
-    else:
-        v += "hrs "
+    # if v == "1":
+    #     v += "hr "
+    # else:
+    #     v += "hrs "
 
-    if minutes%60 != 0 :
+    # if minutes%60 != 0 :
 
-        if v == "0hrs ":
-            v = ""
+    #     if v == "0hrs ":
+    #         v = f'{minutes%60}'
+    #     else:
+    #         v += f' {minutes%60}'
 
-        v += f' {minutes%60}'
-
-        if minutes%60 == 1:
-            v += "min"
-        else:
-             v += "mins"
+    #     if minutes%60 == 1:
+    #         v += "min"
+    #     else:
+    #          v += "mins"
     
     #logger.info(f'format_minutes: {minutes}, {v}')
+
+    hours = math.floor(minutes / 60)
+    mins = minutes % 60
+
+    v = ""
+
+    if hours > 0:
+        v += f'{hours}hr ' if hours == 1 else f'{hours}hrs '
+
+    if mins != 0:
+        v += f'{mins}min' if mins == 1 else f'{mins}mins'
+
+    if v == "":
+        v = "0mins"
 
     return v
